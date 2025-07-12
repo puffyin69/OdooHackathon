f997db439fb4c603db6011c12d64397d87fc5936
kirtan <kirtanthakkar30@gmail.com>
2025-07-12 09:05:56 +0530

    added basic get request

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -3,8 +3,9 @@ import Image from "next/image";
 const Hero = () => {
   return (
     <div className="h-screen items-center justify-center flex flex-col relative">
-      <div className="z-10 w-full flex justify-center absolute top-2">
-        <h1 className="text-5xl font-bold text-white">Rewear</h1>
+      <div className="z-10 w-full flex justify-between absolute top-2">
+        <h1 className="text-xl font-bold text-white">Rewear</h1>
+        <div className=""></div>
       </div>
       <Image src="/hero.png" alt="hero image" fill className="h-screen" />
     </div>
